58745584a32e79f7bd4f786f105bbdc6ff4f2c76
Mister X <martinschulz2000@gmail.com>
2017-10-21 00:17:33 +0200

    add icon

--- LockX/Main.Designer.cs
+++ LockX/Main.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
             this.cbx_screens = new System.Windows.Forms.ComboBox();
             this.lbl_screens = new System.Windows.Forms.Label();
             this.lbl_text = new System.Windows.Forms.Label();
@@ -103,6 +104,7 @@
             this.Controls.Add(this.lbl_screens);
             this.Controls.Add(this.cbx_screens);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Main";
